7a53849f18e66f06df979d5a347a35cbb241ac86
narayanadp <55592290+narayanadp@users.noreply.github.com>
2021-07-29 21:51:09 +0700

    add main

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:takepicture/screens/takepicture.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
